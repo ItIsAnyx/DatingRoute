e080620a2ff23fa894d4a0e36f7a8d8a2b8935c1
Igor Solovev <solov.ig0r.1111@yandex.ru>
2025-12-19 16:31:01 +0300

    Добавлены некоторые улучшения в docker'ы

--- backend-api/src/main/java/com/morzevichka/backend_api/infrastructure/jpa/repository/JpaRouteRepository.java
+++ backend-api/src/main/java/com/morzevichka/backend_api/infrastructure/jpa/repository/JpaRouteRepository.java
@@ -1,9 +1,9 @@
 package com.morzevichka.backend_api.infrastructure.jpa.repository;
 
 import com.morzevichka.backend_api.infrastructure.jpa.entity.RouteEntity;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
